aba2a60ffa47a75ef4e8aa242509aeba0663592a
gunnamrajesh <gunnamrajesh@outlook.com>
2019-12-06 12:38:31 +0530

    initial

---
+++ CalculationSDK/SimpleMath.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CalculationSDK
+{
+    public class SimpleMath
+    {
+        int Add(int a, int b) => a +b;
+
+        int Sub(int large, int small) => large - small;
+
+        int AbsSub(int a, int b) => Math.Abs(a - b);
+    }
+}
